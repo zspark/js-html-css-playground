1591c36fc213577e54536a74b750cafabf0977d0
JerryChaos <jerry-chaos@outlook.com>
2023-04-06 14:36:12 +0800

    2023年4月6日 -- 14点36分

---
+++ flexsearch/mock-data.js
@@ -0,0 +1,11 @@
+window.mockData = Object.freeze([
+    { id: 1, title: 'Web technology for developers', tag: 'tagA,tagB', desc: "new work egg and sand" },
+    { id: 2, title: 'Standard built-in objects', desc: 'New York-Style Bacon Egg and Cheese Sandwich', tag: 'tagA' },
+    { id: 3, title: "JavaScript", desc: 'Bacon Wrapped Cheese Stuffed Meatloaf', tag: 'tagA,tagB' },
+    { id: 4, title: "CSS: Cascading Style Sheets", desc: 'French Yogurt Cake', tag: 'tagA,tagC' },
+    { id: 5, title: 'Using the Fetch API', tag: 'tagE,tagB', desc: "whom you like first work and sand" },
+    { id: 6, title: "Array.prototype.map()", desc: 'Authentic Brazilian Cheese Bread (Pão de Queijo)', tag: 'tagB' },
+    { id: 7, title: "Traversing an HTML table with JavaScript and DOM Interfaces", desc: 'Camarão in into (Brazilian Shrimp Stuffed Pumpkin)', tag: 'tagZ' },
+    { id: 8, title: "Subgrid", desc: 'Parmesan Cheese Muffins', tag: 'tagZ' },
+    { id: 9, title: "AbortSignal", desc: 'Cookie Dough Stuffed Oreos', tag: 'tagC' },
+]);
